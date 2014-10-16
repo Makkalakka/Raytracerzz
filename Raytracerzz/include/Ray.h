0ebd69f35ba241f97bf6992202b508b8e2fa1567
Makkalakka <antal650@student.liu.se>
2014-10-16 16:21:35 +0200

    a small change

--- Raytracerzz/include/Ray.h
+++ Raytracerzz/include/Ray.h
@@ -5,17 +5,17 @@
 class Ray
 {
     public:
-        Ray();
+        Ray(glm::vec3 theStartingPoint, glm::vec3 theDirection, int theImportance, glm::vec3 theColor);
         virtual ~Ray();
 
         //the eye position
-        vector startingPoint;
+        glm::vec3 startingPoint;
         //diection of the random generated ray?
-        vector direction;
+        glm::vec3 direction;
         //how much importance this ray has from the parent ray
         int importance;
         //RGB vector of the ray
-        vector colour;
+        glm::vec3 color;
         //if it finds the wall or left the room (or finds the lightsource?)
         bool finalNode;
         //1 node for intransperent objects, 2 for transperent
@@ -24,7 +24,7 @@ class Ray
         bool insideObject;
 
         //page 6 in the document*
-        void calculateColour();
+        void calculateColor();
         //you shoot shadow rays from the ray-surface intersection point to the point light sources
         void calculateLocalLightingContribution();
         //
